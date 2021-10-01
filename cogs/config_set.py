a3ff5be55f18e53ffae42d022203ce061cc2bb85
Cryptox137 <dev.crypto@hotmail.com>
2021-10-01 18:52:18 +0200

    add /set language command

--- cogs/config_set.py
+++ cogs/config_set.py
@@ -12,5 +12,26 @@ class Config_Set(commands.Cog):
 	async def set(self, inter):
 		pass
 
+	@set.sub_command(name = 'language')
+	async def set_language(
+		self,
+		inter: disnake.ApplicationCommandInteraction,
+		language = commands.param(
+			choices = [
+				disnake.OptionChoice('English', 'english'),
+				disnake.OptionChoice('German', 'german')
+			]
+		)
+	):
+		guild = await Guilds.get(guild_id=inter.guild.id)
+
+		setattr(guild, "lang", language)
+		await guild.save()
+		
+		embed = disnake.Embed(
+			description= "lang changed",
+		)
+		await inter.response.send_message(embed=embed)
+		
 def setup(bot):
 	bot.add_cog(Config_Set(bot))
